795fca592d8b6fd2f5833a06b26f73889796dc66
unknown <shahad0x66@gmail.com>
2024-12-23 18:38:10 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,21 @@
+
+let phone = ['iphone', 'samsung', 'nokia'];
+let found = false;
+
+phone.forEach(e => {
+
+if(e === 'samsung') {
+
+found = true;
+}
+
+})
+
+if(found){
+
+console.log('yes');
+ } else {
+
+console.log('no');
+
+}
